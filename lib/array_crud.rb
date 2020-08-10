391390734d9cc540738c94e6762c90df8e7a5df0
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-10 17:26:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,29 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Beyonce", "Kelly","Michelle", "Letoya"]
 end
-
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
+
 end
 
 def retrieve_element_from_index(array, index_number)
+  array[]
   
 end
 
